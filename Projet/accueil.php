1dcbb8c2424c7f96dea2b7d5a28978e2f397690c
Skyrolight <skyrolight@gmail.com>
2021-10-29 23:58:21 +0200

    1er test Grid

--- Projet/accueil.php
+++ Projet/accueil.php
@@ -27,5 +27,25 @@
                         }
                     } else echo '<a href="seconnecter.php">se connecter</a>';
             ?>
+
+        <div class="container">
+            <div class="page">
+                <div class="sujet">
+                    <div class="grille">
+                        <p>1.</p>
+                        <p>2.</p>
+                        <p>3.</p>
+                        <p>4.</p>
+                        <p>5.</p>
+                        <p>6.</p>
+                        <p>7.</p>
+                        <p>8.</p>
+                        <p>9.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
     </body>
 </html>
