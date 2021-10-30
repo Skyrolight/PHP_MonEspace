3393bbfb2de1d0955ff8a25cef137b0703c7a7dd
Skyrolight <skyrolight@gmail.com>
2021-10-30 12:08:43 +0200

    2eme test Grid

--- Projet/accueil.php
+++ Projet/accueil.php
@@ -8,9 +8,8 @@
         <link rel="stylesheet" href="accueil.css">
     </head>
     <body>
-        
-            <!-- tester si l'utilisateur est connecté -->
-            <?php
+        <!-- tester si l'utilisateur est connecté -->
+        <?php
                 session_start();
                 if(isset($_GET['deconnexion'])) { 
                    if($_GET['deconnexion']=='true') {  
@@ -27,20 +26,39 @@
                         }
                     } else echo '<a href="seconnecter.php">se connecter</a>';
             ?>
-
         <div class="container">
+            
+
             <div class="page">
                 <div class="sujet">
                     <div class="grille">
-                        <p>1.</p>
-                        <p>2.</p>
-                        <p>3.</p>
-                        <p>4.</p>
-                        <p>5.</p>
-                        <p>6.</p>
-                        <p>7.</p>
-                        <p>8.</p>
-                        <p>9.</p>
+                        <?php
+                            require_once('connexion.php');
+                            $requete = $objPdo->query("SELECT COUNT(*) FROM sujet;");
+                            $resultat = $requete->fetchColumn();
+                            $valeur = $resultat;
+        
+                            $tab[] = 0;
+                            for ($i=0; $i < $valeur ; $i++) { 
+                                $tab[$i] = $i++;
+                            }
+
+                            foreach ($tab as $line) {
+                                $line++;
+                                $requete = $objPdo->prepare("SELECT `titresujet`,`textesujet`,`datesujet` FROM sujet WHERE `idsujet`=(?);");
+                                $requete->bindValue(1, $line, PDO::PARAM_INT);
+                                $requete->execute();
+                                $res = $requete->fetch();
+                                $titre = $res['titresujet'];
+                                $texte = $res['textesujet'];
+                                $date = $res['datesujet'];
+                                echo $titre;
+                                echo $texte;
+                                echo $date;
+                            }
+
+                        ?>
+                        
                     </div>
                 </div>
             </div>
